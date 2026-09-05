4bc3c921ba3d0f695ee227f51cd8088205740272
sajica <msjimenezc@unitru.edu.pe>
2026-09-05 15:50:36 -0500

    Update busqueda_secuencial.py
    Puse mi busqueda secuencial

--- src/busqueda_secuencial.py
+++ src/busqueda_secuencial.py
@@ -1,1 +1,17 @@
 #PARTE C
+def busqueda_secuencial(self, codigo_buscado):
+    comparaciones = 0
+    # Recorremos el arreglo lineal de objetos Producto
+    for i, producto in enumerate(self.productos):
+        comparaciones += 1
+        if producto.codigo == codigo_buscado:
+            return {
+                "encontrado": True, 
+                "posicion": i, 
+                "comparaciones": comparaciones
+            }
+    return {
+        "encontrado": False, 
+        "posicion": -1, 
+        "comparaciones": comparaciones
+    }
